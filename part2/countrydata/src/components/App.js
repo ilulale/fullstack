b12876f7a1b5d63cb05ebc124593c2e66a6879ae
kshitij <kshitijtapre@gmail.com>
2020-04-19 20:21:31 +0530

    done country data step 1

--- part2/countrydata/src/components/App.js
+++ part2/countrydata/src/components/App.js
@@ -1,6 +1,39 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
+
+const Showcountry=({newName,countries})=>{
+  const result = countries.filter(cnt=>cnt.name.toLowerCase().includes(newName))
+  if(result.length<9){
+    if(result.length===1){
+      return(
+        <div>
+          <h1>{result[0].name}</h1>
+          <p>Capital : {result[0].capital} <br/>
+          Population : {result[0].population}</p>
+          <h2>Languages</h2>
+          <ul>
+          {result[0].languages.map(lang=><li>{lang.name}</li>)}
+          </ul>
+          <img src={result[0].flag} width='300' height='200'/>
+        </div>
+      )
+    }
+    else{
+      return(
+        <div>
+          {result.map(res=><p>{res.name}</p>)}
+        </div>
+      )
+    }
+  }else{
+    return(
+      <p>Be specific</p>
+    )
+  }
+  }
+
+
 const App = () => {
   const [countries,setCountries] =useState([])
   const [ newName,setnewName ] = useState('')
@@ -10,7 +43,7 @@ const App = () => {
     .then(response=>{
       setCountries(response.data)
     })
-  },[])
+  },[countries])
 
   const handleChange = (event) =>{
     setnewName(event.target.value)
@@ -21,17 +54,18 @@ const App = () => {
       <div>
         <form>
           <div>
-            Find country : <input value={newName} onChange={handleChange} key={'test'}/>
+            Find country : <input value={newName} onChange={handleChange} />
           </div>
         </form>
       </div>
     )
   }
 
+
   return(
     <div>
-    <p>Meow</p>
-    <Finder/>
+    {Finder()}
+    <Showcountry newName={newName} countries={countries} />
     </div>
   )
 
