9f3dfa76eab2f63d44decb2e9cd15c032734b325
kshitij <kshitijtapre@gmail.com>
2020-04-23 22:02:36 +0530

    done with 2.16 step 8,stuck on step 9 rerender phonebook

--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,14 +1,19 @@
 import React from 'react' 
 import phoneService from './services/phones'
+import App from './App'
 
 
 const Phonebook=({name,number,did})=>{
     return(
         <p>{name} {number} 
-        <button>delete</button></p>
+        <button onClick={()=>ohrio(did)}>delete</button></p>
     )
 }
 
-
+const ohrio = delid =>{
+    phoneService
+    .delrio(delid)
+    .then(window.alert('deleted!'))
+}
 
 export default Phonebook
