2b2057da5fc95ea5f597e4f3d6dd1710af642e9a
kshitij <kshitijtapre@gmail.com>
2020-04-24 20:59:48 +0530

    completed 2.19 phonebook step 11

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -6,31 +6,48 @@ const App = () => {
   const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ errorMessage, setErrorMessage ] = useState(null)
 
-
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+  
   useEffect(()=>{
     phoneService
     .getAll()
     .then(response=>{
       setPersons(response.data)
-    })
+      })
   },[])
 
   const addNumber=(event)=>{
     event.preventDefault()
+    //console.log(persons)
     const phoneObject = {
       name: newName,
       number: newNumber,
       id: persons.length + 1
     }
+    //console.log(persons.map(person=>person.name))
     if(persons.some(person=>person.name===newName)){
-      window.alert(`${newName} already exists`)}
+      setErrorMessage(`${newName} already exists`)
+      setTimeout(()=>{setErrorMessage(null)},2000)
+    }
     else{
       setPersons(persons.concat(phoneObject))
       phoneService
       .create(phoneObject)
       .then(response=>{
-          window.alert(`${phoneObject.name} added to db`)
+        setErrorMessage(`${newName} added to db`)
+        setTimeout(()=>{setErrorMessage(null)},2000)
       })
       setNewName('')
       setNewNumber('')
@@ -57,12 +74,16 @@ const App = () => {
 const ohrio = delid =>{
     phoneService
     .delrio(delid)
-    .then(window.alert('deleted!'))
+    .then(
+      setErrorMessage('Deleted'),
+      setTimeout(()=>{setErrorMessage(null)},2000)
+    )
     setPersons(persons.filter(person=>person.id!==delid))
 }
 
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>Phonebook</h2>
       <Form addNumber={addNumber} newName={newName} handleName={handleName} newNumber={newNumber} handleNumber={handleNumber} />
       <h2>Numbers</h2>
